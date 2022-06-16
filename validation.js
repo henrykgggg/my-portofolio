e43b8c28bf074c64a2f18d0bdc751a2d102db293
cynthianwannah <chinwendunwannah@yahoo.com>
2022-06-16 09:00:19 +0100

    added javascript to my form

--- validation.js
+++ validation.js
@@ -3,3 +3,12 @@ const Email = document.getElementById('email');
 const Errormessage = document.getElementById('form-error');
 Errormessage.textContent = 'Message not sent! Please write using only the small letters. Thank you.';
 
+function validationForm(e) {
+  if (Email.value !== Email.value.toLowerCase()) {
+    Errormessage.style.display = 'block';
+    e.preventDefault();
+  } else {
+    Errormessage.style.display = 'none';
+  }
+}
+formbutton.addEventListener('click', validationForm);

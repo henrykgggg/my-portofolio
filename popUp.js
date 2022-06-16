8f818aa339438e70c6f58e3f4eaa49c16963694e
henry <88551100+henrykgggg@users.noreply.github.com>
2022-06-16 16:48:24 +0200

    added error message

--- popUp.js
+++ popUp.js
@@ -2,7 +2,7 @@ const projects = [
     {
       title: 'TONIC 1',
       prof:['Canopy','back end Dev','2015'],
-      src: "img/Snapshoot.png",
+      src: "images/snapshoot4.png",
       description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe."],
       technologies: ['html', 'Ruby', 'css', 'Github'],
       seeLiveLink: '#',
@@ -11,7 +11,7 @@ const projects = [
     {
       title: 'TONIC 2',
       prof:['Canopy','back end Dev','2015'],
-      src: "img/Snapshoot.png",
+      src: "images/portfolio_4.png",
       description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe."],
       technologies: ['html', 'Ruby ', 'css', 'Github'],
       seeLiveLink: '#',
@@ -20,7 +20,7 @@ const projects = [
     {
       title: 'TONIC 3',
       prof:['Canopy','back end Dev','2015'],
-      src: "img/Snapshoot.png",
+      src: "images/portfolio_4.png",
       description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe."],
       technologies: ['html', 'Ruby', 'css', 'Github'],
       seeLiveLink: '#',
@@ -29,7 +29,7 @@ const projects = [
     {
       title: 'TONIC 4',
       prof:['Canopy','back end Dev','2015'],
-      src: "img/Snapshoot.png",
+      src: "images/Snapshoot.png",
       description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe."],
       technologies: ['html', 'Ruby', 'css', 'Github'],
       seeLiveLink: '#',
@@ -60,7 +60,7 @@ const projects = [
 
         </div>
         <div class="modal-image">
-            <img src =${projects[index].src}
+            <img src =${projects[index].src}/>
         </div>
         </div>
         <div class="modal-main">
@@ -82,10 +82,10 @@ const projects = [
   </ul>
 </div>
 <button class="click-btn">
-<a href="${projects[index].seeLiveLink}">See Live <img src="img/github.png" alt="See live"></i></a>
+<a href="${projects[index].seeLiveLink}">See Live <img src="images/github.png" alt="See live"></i></a>
 </button>
 <button class="click-btn">
-<a href="${projects[index].seeSourceLink}">See Source <img src="img/github.png" alt="Github icon"></a>
+<a href="${projects[index].seeSourceLink}">See Source <img src="images/github.png" alt="Github icon"></a>
 </button>
 </div>
 </div>
@@ -99,3 +99,64 @@ const projects = [
         popUp.style.display = 'none';
     }
   });
+
+  function mobile(index){
+    popUp.style.display='block';
+    popUp.appendChild(projectDetailCard);
+    projectDetailCard.innerHTML=`
+    <div class="modal-content">
+        <div class="modal-head">
+            <h2>${projects[index].title}</h2>
+            <i class="fa-solid fa-xmark" id="close-popUp"></i>
+        </div>
+        <div class="modal-prof">
+            <span>${projects[index].prof}</span>
+
+        </div>
+        <div class="modal-image">
+            <images src =${projects[index].src}
+        </div>
+        </div>
+  <div class="modal-description">
+    <p>${projects[index].description}</p>
+  </div>
+  <div class="modal-technolog">
+  <ul class="project-lists">
+  
+    <li>${projects[index].technologies[0]}</li>
+    
+    <li>${projects[index].technologies[1]}</li>
+
+    <li>${projects[index].technologies[2]}</li>
+ 
+    <li>${projects[index].technologies[3]}</li>
+ 
+  </ul>
+</div>
+<button>
+<a href="${projects[index].seeLiveLink}">See Live <img src="img/github.png" alt="See live"></i></a>
+</button>
+<button>
+<a href="${projects[index].seeSourceLink}">See Source <img src="img/github.png" alt="Github icon"></a>
+</button>
+</div>
+    </div>
+    </section>`
+    
+  }
+
+  document.addEventListener('click',(event)=>{
+    if(event.target.id === 'close-popUp'){
+       popUp.style.display='none';
+    }
+});
+
+for (let i = 0; i< btns.length; i += 1) {
+    btns[i].addEventListener('click', () => {
+         mobile(i);
+         
+    });
+    btns[i].addEventListener('click', () => {
+        desktop(i);
+    });
+}
---
+++ validation.js
@@ -0,0 +1,4 @@
+const formbutton = document.getElementById('form-button');
+const Email = document.getElementById('email');
+const Errormessage = document.getElementById('form-error');
+Errormessage.textContent = 'Message not sent! Please write using only the small letters. Thank you.';
